1e9a1aee361f5d3ba91e79d3f129f016328a2d46
Erik Zilliacus <erik.zilliacus@gmail.com>
2021-01-02 17:47:55 +0200

    remove unused saadc and ppi stuff

--- main.c
+++ main.c
@@ -64,16 +64,12 @@
 #include "nrf_log_default_backends.h"
 
 // SAADC
-#include "nrfx_saadc.h"
-#include "nrfx_ppi.h"
-#include "nrf_drv_ppi.h"
 #include "nrfx.h"
 #include "nrfx_timer.h"
 #include "boards.h"
 #include "app_error.h"
 #include "nrf_delay.h"
 #include "app_util_platform.h"
-#include "nrfx_rtc.h"
 
 // Capsense
 #include "nrf_drv_csense.h"
@@ -81,11 +77,6 @@
 #define USE_LED_INDICATION 0
 
 #define SAMPLES_IN_BUFFER 1
-
-static const nrfx_rtc_t     m_rtc = NRFX_RTC_INSTANCE(2);
-static nrf_saadc_value_t     m_buffer_pool[2][SAMPLES_IN_BUFFER];
-static nrf_ppi_channel_t     m_ppi_channel;
-static uint32_t              m_adc_evt_counter;
 
 #define APP_BLE_CONN_CFG_TAG            1                                  /**< A tag identifying the SoftDevice BLE configuration. */
 
@@ -320,132 +311,6 @@ static void ble_stack_init(void)
 
 
 /* BLE end */
-
-/* RTC */
-
-static void rtc_handler(nrfx_rtc_int_type_t int_type)
-{
-
-}
-
-/* RTC end */
-
-/*SAADC*/
-
-
-void timer_handler(nrf_timer_event_t event_type, void * p_context)
-{
-
-}
-
-
-void saadc_sampling_event_init(void)
-{
-  ret_code_t err_code;
-
-  err_code = nrf_drv_ppi_init();
-  APP_ERROR_CHECK(err_code);
-
-  // RTC SETUP
-  nrfx_rtc_config_t rtc_config = NRFX_RTC_DEFAULT_CONFIG;
-  rtc_config.prescaler = 4095; // Set prescaler to 4095 -> frequency = 8 Hz
-  rtc_config.interrupt_priority = NRFX_RTC_DEFAULT_CONFIG_IRQ_PRIORITY;
-  rtc_config.reliable = NRFX_RTC_DEFAULT_CONFIG_RELIABLE;                  
-  rtc_config.tick_latency = NRFX_RTC_US_TO_TICKS(NRFX_RTC_MAXIMUM_LATENCY_US,NRFX_RTC_DEFAULT_CONFIG_FREQUENCY);
-  err_code = nrfx_rtc_init(&m_rtc, &rtc_config, rtc_handler);
-  APP_ERROR_CHECK(err_code);
-  //Set compare channel 0 to trigger when value reaches 8 -> at 1 Hz
-  err_code = nrfx_rtc_cc_set(&m_rtc,0,40,false);
-  APP_ERROR_CHECK(err_code);
-
-  //Power on RTC instance
-  nrfx_rtc_enable(&m_rtc);
-
-  uint32_t rtc_compare_event_addr = nrfx_rtc_event_address_get(&m_rtc, NRF_RTC_EVENT_COMPARE_0);
-  uint32_t rtc_clear_task_addr = nrfx_rtc_task_address_get(&m_rtc, NRF_RTC_TASK_CLEAR);
-  uint32_t saadc_sample_event_addr = nrfx_saadc_sample_task_get();
-
-  /* Setup ppi channel so that: RTC COMPARE -> TASK: sampling in SAADC + FORK: RTC clear*/
-  err_code = nrfx_ppi_channel_alloc(&m_ppi_channel);
-  APP_ERROR_CHECK(err_code);
-  //nrfx_ppi_channel_assign(ppi_channel, event, task_to_activate_during_event);
-  err_code = nrfx_ppi_channel_assign(m_ppi_channel, rtc_compare_event_addr, saadc_sample_event_addr);
-  APP_ERROR_CHECK(err_code);
-  // Assign a second task, i.e. a "fork" to be executed in addition to primary task
-  err_code = nrfx_ppi_channel_fork_assign(m_ppi_channel, rtc_clear_task_addr);
-  APP_ERROR_CHECK(err_code);
-}
-
-
-void saadc_sampling_event_enable(void)
-{
-  ret_code_t err_code = nrfx_ppi_channel_enable(m_ppi_channel);
-
-  APP_ERROR_CHECK(err_code);
-}
-
-
-void saadc_callback(nrfx_saadc_evt_t const * p_event)
-{
-  if (p_event->type == NRFX_SAADC_EVT_DONE)
-  {
-    ret_code_t err_code;
-
-    err_code = nrfx_saadc_buffer_convert(p_event->data.done.p_buffer, SAMPLES_IN_BUFFER);
-    APP_ERROR_CHECK(err_code);
-
-    int i;
-    //NRF_LOG_INFO("ADC event number: %d", (int)m_adc_evt_counter);
-
-    for (i = 0; i < SAMPLES_IN_BUFFER; i++)
-    {			 
-      convert16to8((uint16_t)p_event->data.done.p_buffer[i], m_moisture_level);
-      //NRF_LOG_INFO("%d", p_event->data.done.p_buffer[i]);
-    }
-    m_adc_evt_counter++;
-  }
-  //nrf_drv_gpiote_out_toggle(LED_3);
-  advertising_init();
-  advertising_start();
-}
-
-
-void saadc_init(void)
-{
-  ret_code_t err_code;
-
-  nrfx_saadc_config_t saadc_config;
-  saadc_config.resolution = NRF_SAADC_RESOLUTION_12BIT;                                 
-  saadc_config.oversample = NRF_SAADC_OVERSAMPLE_8X;
-  saadc_config.interrupt_priority = APP_IRQ_PRIORITY_LOW;
-  saadc_config.low_power_mode = true;
-
-  nrf_saadc_channel_config_t channel_config; //= NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN0);
-  channel_config.resistor_p = NRF_SAADC_RESISTOR_DISABLED;
-  channel_config.resistor_n = NRF_SAADC_RESISTOR_DISABLED;
-  channel_config.gain       = NRF_SAADC_GAIN1_6;
-  channel_config.reference  = NRF_SAADC_REFERENCE_INTERNAL;
-  channel_config.acq_time   = NRF_SAADC_ACQTIME_10US;
-  channel_config.mode       = NRF_SAADC_MODE_SINGLE_ENDED;
-  channel_config.burst      = NRF_SAADC_BURST_ENABLED; // If ON: Executes all Oversample times as quickly as possible on each activation
-  channel_config.pin_p      = NRF_SAADC_INPUT_AIN2;
-  channel_config.pin_n      = NRF_SAADC_INPUT_DISABLED;
-
-  err_code = nrfx_saadc_init(&saadc_config, saadc_callback);
-  APP_ERROR_CHECK(err_code);
-
-  err_code = nrfx_saadc_channel_init(0, &channel_config);
-  APP_ERROR_CHECK(err_code);
-
-  err_code = nrfx_saadc_buffer_convert(m_buffer_pool[0], SAMPLES_IN_BUFFER);
-  APP_ERROR_CHECK(err_code);
-
-  err_code = nrfx_saadc_buffer_convert(m_buffer_pool[1], SAMPLES_IN_BUFFER); // "Extra" buffer to write to when buffer 1 is full
-  APP_ERROR_CHECK(err_code);
-}
-
-
-// SAADC END
 
 /*
 static void log_init(void)
